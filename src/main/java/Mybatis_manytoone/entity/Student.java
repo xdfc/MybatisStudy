1af904e16eb59782cf9afe5fa68fd57c16a53773
fangcong <xdufc@qq.com>
2020-11-08 16:07:09 +0800

    多张表查询——多对一的查询

---
+++ src/main/java/Mybatis_manytoone/entity/Student.java
@@ -0,0 +1,40 @@
+package Mybatis_manytoone.entity;
+
+public class Student {
+    private int id;
+    private String name;
+    private Teacher teacher;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", teacher=" + teacher +
+                '}';
+    }
+}
